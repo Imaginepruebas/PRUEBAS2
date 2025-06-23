be6387bb0cb57039de30851f685c1ec3494ae967
FABIO HERNAN FORERO GARZON <79220621@bolivar.com>
2025-06-23 13:59:12 -0500

    Actualizaci├│n autom├ítica del informe

--- subir_git.bat
+++ subir_git.bat
@@ -1,6 +1,14 @@
 @echo off
 cd /d "%~dp0"
+echo Ejecutando git add...
 git add .
+
+echo Ejecutando git commit...
 git commit -m "Actualización automática del informe"
+
+echo Subiendo a GitHub...
 git push
+
+echo ----------------------
+echo Proceso terminado. Presiona una tecla para cerrar.
 pause
